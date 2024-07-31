db4b51a3afe48680266c3decfe13f547028fb8e9
LUCIFER <102866831+LuciferX-Devil@users.noreply.github.com>
2024-07-31 12:30:07 +0530

    Create Palindrome.java

---
+++ JAVA/Basics/Palindrome.java
@@ -0,0 +1,24 @@
+import java.lang.*;
+import java.util.*;
+
+class Palindrome {
+    public static void Palind(String number) {
+        int check = 0;
+        int j = number.length() - 1;
+        for (int i = 0; i < number.length(); i++) {
+            if (number.charAt(i) == number.charAt(j)) { check++; }
+            j--;
+        }
+        if (check == number.length()) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not a palindrome");
+        }
+    }
+
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+        String number = input.next();
+        Palind(number);
+    }
+}
